2fa75dae7e680cba04dbdaa24cab6e0edb5c596e
stefan g <116798342+stefangoina@users.noreply.github.com>
2024-08-08 01:41:10 +0300

    submit button adds book into library with corresponding information

--- script.js
+++ script.js
@@ -10,3 +10,37 @@ jsCloseBtn.addEventListener("click", (e) => {
   e.preventDefault();
   dialog.close();
 });
+
+// grabbing inputs from dialog
+let bookTitle = document.getElementById("title");
+let bookAuthor = document.getElementById("author");
+let numberPages = document.getElementById("pages");
+let isRead = document.getElementById("isread");
+let submitBtn = document.getElementById("submit-book");
+
+// storage and constructor
+
+const library = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+// build book from html input
+submitBtn.addEventListener("click", () => {
+  const newBook = new Book(
+    bookTitle.value,
+    bookAuthor.value,
+    numberPages.value,
+    isRead.checked
+  );
+
+  library.push(newBook);
+  dialog.reset();
+  console.log(library);
+});
+
+function addToLibrary() {}
